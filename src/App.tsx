f611cf873624abd922128696f9b3bcf4191eb82c
Katepiss Sriburapa <artcoding2544@gmail.com>
2024-02-18 21:05:51 +0700

    feat: background color change to first one after submit

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,31 @@ import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 import ToDo from './Models';
+import ToDoList from './components/ToDoList';
 const App: React.FC = () => {
   const [toDo, setToDo] = useState<string>('');
   const [toDos, setToDos] = useState<ToDo[]>([]);
+
   const handleApp = (e: React.FormEvent) => {
     if (toDo) {
-      setToDos([...toDos, { id: Date.now(), todo: toDo, isDone: false }]);
+      setToDos([...toDos, { id: Date.now(), todo: toDo, isDone: false }]); //combine exiting toDo and new to do by using spread operator
+      setToDo(''); //clear the input field
     }
 
     e.preventDefault();
   };
 
-  console.log(...toDos);
-  console.log(toDo);
+  console.log('All todos', toDos);
+  console.log('The last todo:', toDo);
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField toDo={toDo} setToDo={setToDo} handleAdd={handleApp} />
+      <ToDoList setToDos={setToDos} toDos={toDos} />
+      {toDos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 };
